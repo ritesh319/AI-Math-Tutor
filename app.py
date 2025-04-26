95edd6ab8469c10f910d679006b1c66d86eb64ba
Ritesh kumar <riteshk3113@gmail.com>
2025-04-26 20:19:19 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -114,5 +114,6 @@ def upload_audio():
         logging.error(f"Error occurred in /upload_audio:", exc_info=True)
         return jsonify({"error": "Server error processing audio."}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+# it was commented out for deployment. If u want to run locally on your system, remove the comments and use it.
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5000)
